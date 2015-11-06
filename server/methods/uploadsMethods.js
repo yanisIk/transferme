a3a81e97061d880b222966381928971346e1d7ce
Yanis <yanis.ikene.1@etsmtl.net>
2015-11-06 00:16:25 -0500

    Uploads.create method

--- server/methods/uploadsMethods.js
+++ server/methods/uploadsMethods.js
@@ -1,5 +1,17 @@
 Meteor.methods({
   "Uploads.create" : function (options) {
-    
+  	//TODO Validate options
+  	additionalFields = {
+  		userId: this.userId || "",
+  		sessionId: this.connection.id,
+  		uploadedAt: new Date(),
+  	}
+  	options = _.extend(options, additionalFields);
+  	Uploads.insert(options);
+  	
+  	this.unblock();
+  	if (options.emailTo) {
+  		//Send email with download link
+  	}
   }
 });
